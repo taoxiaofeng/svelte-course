31cf3d64b042fc97b3b03e9b222b0f85139551bc
taoxiaofeng <taoxiaofeng55@qq.com>
2023-03-25 15:54:13 +0800

    init project

---
+++ src/lib/State.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+  let name = '张三';
+</script>
+
+
+<div>
+  <input bind:value={name} />
+  <p>你好，{name}</p>
+</div>
+
+<style>
+  div {
+    border: 1px solid #ccc;
+    padding: 10px;
+  }
+</style> 
